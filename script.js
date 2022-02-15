951486694b3d2e6396f1b0b72b2fa9c124c13fb9
ernesto-1998 <ernestoalonso1998@gmail.com>
2022-02-15 15:50:35 -0600

    improvements on cards

---
+++ script.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data(){
+        return {
+            contenidoCards : [
+                {url: "./img/card1.png", titulo: "Promociones"},
+                {url: "./img/card2.png", titulo: "La Tienda de Pollito"},
+                {url: "./img/card3.png", titulo: "Camperitos Doritos"},
+                {url: "./img/card4.png", titulo: "Pollo"},
+                {url: "./img/card5.png", titulo: "Camperitos, Alitas y Pechuguitas"},
+                {url: "./img/card6.png", titulo: "Pizza"},
+                {url: "./img/card7.png", titulo: "Sándwiches, Hamburguesas y Burritos"},
+                {url: "./img/card8.png", titulo: "Ensaladas y Tazón"},
+                {url: "./img/card9.png", titulo: "Cenas"},
+                {url: "./img/card10.png", titulo: "Loncherita"},
+                {url: "./img/card11.png", titulo: "Desayunos"},
+                {url: "./img/card12.png", titulo: "Postres"},
+                {url: "./img/card13.png", titulo: "bebidas"},
+                {url: "./img/card14.png", titulo: "Extras"}
+            ]
+        }
+
+    }
+  })
